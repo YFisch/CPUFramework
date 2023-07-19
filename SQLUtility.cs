251571f2d48ac63c420b4da4a1f46cbe19d3e068
YFisch <yfisch381@gmail.com>
2023-07-19 15:24:28 -0400

    Initial Commit

---
+++ SQLUtility.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+
+        public static DataTable GetDatatable(string sqlstatement)
+        {
+            Debug.Print(sqlstatement);
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            //DisplayMessage("Conn Status", conn.State.ToString());
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
