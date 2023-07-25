a69d8e32baa6900c310f8c7ea555a216e76f15a2
YFisch <yfisch381@gmail.com>
2023-07-25 15:42:51 -0400

    Intergrate Sprocs with .Net

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -8,35 +8,50 @@ namespace CPUFramework
     {
         public static string ConnectionString = "";
 
-        public static DataTable GetDatatable(string sqlstatement)
+        public static SqlCommand GetSQLCommand(string sprocname)
         {
-            Debug.Print(sqlstatement);
+            SqlCommand cmd;
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+                cmd = new SqlCommand(sprocname, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            return cmd;
+        }
+
+        public static DataTable GetDataTable(SqlCommand cmd)
+        {
+            Debug.Print("--------" + Environment.NewLine + cmd.CommandText);
             DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-            //DisplayMessage("Conn Status", conn.State.ToString());
-            var cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            var dr = cmd.ExecuteReader();
-            dt.Load(dr);
-
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                SqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr);
+            }
             SetAllColumnsAllowNull(dt);
-
             return dt;
+        }
+
+        public static DataTable GetDataTable(string sqlstatement)
+        {
+            return GetDataTable(new SqlCommand(sqlstatement));
         }
 
         public static void ExecuteSQL(string sqlstatement)
         {
-            GetDatatable(sqlstatement);
+            GetDataTable(sqlstatement);
         }
 
         public static int GetFirstColumnFirstRowValue(string sql)
         {
             int n = 0;
 
-            DataTable dt = GetDatatable(sql);
+            DataTable dt = GetDataTable(sql);
             if(dt.Rows.Count > 0 && dt.Columns.Count > 0)
             {
                 if (dt.Rows[0][0] != DBNull.Value)
